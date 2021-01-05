b5d41d3b31b0cbce2974868eb0a6bf2a8fe739a7
Robert den Blaauwen <rdenblaauwen@gmail.com>
2021-01-05 14:59:33 +0100

    basic form binding

--- src/app/kku-customer/user-form/user-form.component.ts
+++ src/app/kku-customer/user-form/user-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'kku-user-form',
@@ -6,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-form.component.scss'],
 })
 export class UserFormComponent {
+  @Input('user') userModel?: User;
+  @ViewChild('form') signupForm?: NgForm;
+
+  public default = 'kek';
+
+  // onSubmit(form: NgForm): void {
+  //   console.log('submitted', form);
+  // }
+
   onSubmit(): void {
-    console.log('submitted');
+    // console.log('submitted', form);
+    console.log('submitted', this.signupForm);
   }
 }
